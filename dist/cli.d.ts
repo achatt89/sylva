54b84369326af86f7c66accc9c952d4607ee925b
Abhijit Chatterjee <abhijitchatterjee@Abhijits-MacBook-Pro.local>
2026-02-25 22:27:53 +0400

    1.0.2

--- dist/cli.d.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-export declare function resolveRepositoryTarget(args: any): Promise<{
-    repoUrl: string | null;
-    localPath: string | null;
-    repoName: string;
-}>;
--- dist/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-console.log("Sylva initialized");
